3b2f93f9d675b8c6910977596f699b690b8e0fb2
deaconTown <gnerddavia@gmail.com>
2019-08-29 06:38:44 -0500

    changed the controller route string from 'controller' to 'todo'

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -7,7 +7,7 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[todo]")]
     [ApiController]
     public class TodoController : ControllerBase
     {
